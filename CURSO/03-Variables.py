942439f8839069925c60f93577d732d98d4a142b
Jonathan20061613 <andressguilera2006@gmail.com>
2026-07-19 16:16:35 -0500

    Operadores y Variables

--- CURSO/03-Variables.py
+++ CURSO/03-Variables.py
@@ -19,3 +19,24 @@ print('Este es el valor de:', my_bool_variable)
 # Variables en uns sola linea 
 name, surname, age, alias = 'Jonathan', 'Jimenez', 20, 'J.A.J.A'
 print('Me llamo:', name, surname, '. Mi edad es:', age, '. Y mi alias es:', alias)
+
+# Inputs
+#name = input('Cual es tu nombre?')
+#age = input('Cual es tu edad?')
+
+#print(name)
+#print(age)
+
+name = 35
+age = 'Jonathan'
+print(name)
+print(age)
+
+address: str = 'Mi direccion'
+address = True
+address = 5
+address = 1.5
+print(address)
+
+print(type(address))
+
---
+++ CURSO/04-Operadores.py
@@ -0,0 +1,29 @@
+# Operadores
+# Operadores Aritmeticos
+print(3 + 4) # Suma
+print(3 - 4) # Resta
+print(3 * 4) # Multiplicacion
+print(3 / 4) # Division
+print(10 % 3) # Modulo
+print(10 // 3) # Division Entera
+print(2 ** 3) # Potencia
+
+print('Hola ' + 'Python') 
+print('Hola ' + str(5))
+print('Hola ' * 5)
+print('Hola ' * (2 ** 3))
+
+my_float = 2.5 * 2
+print('Hola ' * int(my_float))
+
+print('---------------------------------------------------------------')
+
+# Operadores Comparativos
+print(3 > 4) # Mayor que
+print(3 < 4) # Menor que
+print(3 >= 4) # Mayor o Igual que
+print(3 <= 4) # Menor o Igual que
+print(3 == 4) # Igual que
+print(3 != 4) # Dieferente de
+
+print('Hola' > '')
